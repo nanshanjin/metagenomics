2d1455d9f8620b8db6a8654938638c69f4f3d043
nanshanjin <nanshangogo@163.com>
2019-09-30 14:07:09 +0800

    add kraken2

---
+++ kraken2.sh
@@ -0,0 +1,3 @@
+kraken2-2.0.8-beta/kraken2 -db /kraken2/metadb  --threads 8 --fastq-input --gzip-compresse --paired hisat.fq.1.gz hisat.fq.2.gz --report kraken2.report.xls
+##mpa format result
+kraken2-2.0.8-beta/kraken2 -db /kraken2/metadb  --threads 8 --fastq-input --gzip-compresse --paired hisat.fq.1.gz hisat.fq.2.gz --report kraken2.report.xls --use-mpa-style
